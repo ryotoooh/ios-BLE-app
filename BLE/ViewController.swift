64668d50de2225f85e7d95ae427a474b0fbd3dbf
Ryota H <r.hisanabe@gmail.com>
2020-11-01 18:18:23 +1100

    Initial Commit

---
+++ BLE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BLE
+//
+//  Created by Ryota on 1/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BLETests/BLETests.swift
@@ -0,0 +1,33 @@
+//
+//  BLETests.swift
+//  BLETests
+//
+//  Created by Ryota on 1/11/20.
+//
+
+import XCTest
+@testable import BLE
+
+class BLETests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
